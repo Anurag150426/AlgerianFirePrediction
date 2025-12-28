1574941dfc86c6b522b225b88db413c4d4e09ddc
Anurag Kumar <kumaranu8975@gmail.com>
2025-12-29 01:42:54 +0530

    Added Flask app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+import pickle
+from flask import Flask, request, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+model = pickle.load(open('models/ridge_model.pkl','rb'))
+scaler = pickle.load(open("models/scaler.pkl","rb"))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/predict_datapoint", methods=["GET", "POST"])
+def predict_datapoint():
+    
+    if request.method == "POST":
+        Temperature = float(request.form.get("Temperature"))
+        RH = float(request.form.get("RH"))
+        Ws = float(request.form.get("Ws"))
+        Rain = float(request.form.get("Rain"))
+
+        FFMC = float(request.form.get("FFMC"))
+        DMC = float(request.form.get("DMC"))
+        ISI = float(request.form.get("ISI"))
+
+        Classes = float(request.form.get("Classes"))
+        Region = float(request.form.get("Region"))
+
+        new_data = scaler.transform([[Temperature, RH, Ws, Rain,
+                                      FFMC, DMC, ISI,
+                                      Classes, Region]])
+
+        result = model.predict(new_data)[0]
+
+        return render_template("home.html", result=result)
+    else:
+        return render_template("home.html", result="Something went wrong")
+
+if __name__ == "__main__":
+    app.run (host="0.0.0.0", port=5000)
