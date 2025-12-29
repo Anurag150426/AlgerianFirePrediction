0d4729f6a356cc3c2568d347b34fc1c8b6686769
Anurag Kumar <kumaranu8975@gmail.com>
2025-12-29 22:44:50 +0530

    app updated

--- app.py
+++ app.py
@@ -31,3 +31,5 @@ def home():
         return render_template("home.html", result=result)
 
     return render_template("home.html")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
