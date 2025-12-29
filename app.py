ff8689313adcb5ced9b5565e41e9793ad7fb7a71
Anurag Kumar <kumaranu8975@gmail.com>
2025-12-29 23:06:32 +0530

    app updated

--- app.py
+++ app.py
@@ -11,24 +11,26 @@ scaler = pickle.load(open("models/scaler.pkl","rb"))
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-        Temperature = float(request.form.get("Temperature"))
-        RH = float(request.form.get("RH"))
-        Ws = float(request.form.get("Ws"))
-        Rain = float(request.form.get("Rain"))
+        try:
+            Temperature = float(request.form.get("Temperature", 0))
+            RH = float(request.form.get("RH", 0))
+            Ws = float(request.form.get("Ws", 0))
+            Rain = float(request.form.get("Rain", 0))
+            FFMC = float(request.form.get("FFMC", 0))
+            DMC = float(request.form.get("DMC", 0))
+            ISI = float(request.form.get("ISI", 0))
+            Classes = float(request.form.get("Classes", 0))
+            Region = float(request.form.get("Region", 0))
 
-        FFMC = float(request.form.get("FFMC"))
-        DMC = float(request.form.get("DMC"))
-        ISI = float(request.form.get("ISI"))
+            new_data = scaler.transform([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, Classes, Region]])
+            result = model.predict(new_data)[0]
 
-        Classes = float(request.form.get("Classes"))
-        Region = float(request.form.get("Region"))
-        new_data = scaler.transform([[Temperature, RH, Ws, Rain,
-                                      FFMC, DMC, ISI,
-                                      Classes, Region]])
-        result = model.predict(new_data)[0]
-
-        return render_template("home.html", result=result)
+            return render_template("home.html", result=round(result, 2))
+        
+        except Exception as e:
+            return f"Error: {str(e)}"
 
     return render_template("home.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
